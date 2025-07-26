1caa3739a68500735b6cf704c4d562f5429f0c94
Avik-creator <avikm744@gmail.com>
2025-07-26 17:14:37 +0530

    Added Changes.

--- backend/src/controllers/cronCrontroller.ts
+++ backend/src/controllers/cronCrontroller.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { Context } from "hono";
 import cron from "node-cron";
 import {
--- backend/src/services/cronJobService.ts
+++ backend/src/services/cronJobService.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import cron, { ScheduledTask } from "node-cron";
 import {
   execute,
